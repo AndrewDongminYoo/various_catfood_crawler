c2b441f44f28d24d5f1377fd5dada375bf680204
Dongmin Yu <ydm2790@gmail.com>
2021-07-01 20:46:17 +0900

    mamacat

--- mamacat_food.py
+++ mamacat_food.py
@@ -7,7 +7,7 @@ driver.set_window_size(width=1600, height=900)
 driver.minimize_window()
 driver.implicitly_wait(15)
 
-with open('./MAMACAT_url.txt', 'r', encoding='utf8', newline="") as input:
+with open('./MAMACATURL.txt', 'r', encoding='utf8', newline="") as input:
     formulas_list = []
     for idx, line in enumerate(input.readlines()):
         driver.get(line)
@@ -15,7 +15,7 @@ with open('./MAMACAT_url.txt', 'r', encoding='utf8', newline="") as input:
         title = driver.find_element(
             By.XPATH, '//*[@id="contents"]/div[2]/div[2]/div[2]/div[1]/h2').text
         image = driver.find_element(
-            By.XPATH, '//*[@id="contents"]/div[2]/div[2]/div[1]/div[1]/div/a/img').get_attr('src')
+            By.XPATH, '//*[@id="contents"]/div[2]/div[2]/div[1]/div[1]/div/a/img').get_attribute('src')
         diction['id'] = idx
         diction['name'] = title
         diction['image'] = image
@@ -23,8 +23,9 @@ with open('./MAMACAT_url.txt', 'r', encoding='utf8', newline="") as input:
             By.XPATH, '//*[@id="contents"]/div[2]/div[2]/div[2]/div[2]/table/tbody')
         head = table.find_elements(By.TAG_NAME, 'th')
         data = table.find_elements(By.TAG_NAME, 'td')
-        for one, two in map(head, data):
+        for one, two in zip(head, data):
             diction[one.text] = two.text
+            print(two.text)
         formulas_list.append(diction)
 with open('./mamacat.json', 'w', encoding='utf8', newline="") as output:
     json.dump(formulas_list, output, ensure_ascii=False, indent=4)
