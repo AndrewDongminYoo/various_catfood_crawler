9095b0e080deff5b983b679136cc89d2b4d52366
AndrewDongminYoo <ydm2790@gmail.com>
2021-08-24 03:40:14 +0900

    evolve & almonature fix

--- utils/AlmoNature.py
+++ utils/AlmoNature.py
@@ -27,7 +27,7 @@ def get_text_by_css(css):
     targets = driver.find_elements(By.CSS_SELECTOR, css)
     if len(targets) == 0:
         return None
-    if len(targets) == 1:
+    elif len(targets) == 1:
         return targets[0].text.replace("\n", " ")
     for t in targets:
         if t.text:
@@ -58,20 +58,20 @@ def get_text_by_css(css):
 # input_file = open("./data/AlmoNature.json", mode="w", newline="", encoding="utf-8")
 # json.dump(obj=almo, fp=input_file, indent=3, ensure_ascii=False)
 # input_file.close()
-input_file2 = open("./data/AlmoNature.json", mode="r+", encoding="utf-8")
-almo = json.load(input_file2)
+input_file = open("./data/AlmoNature.json", mode="r", encoding="utf-8")
+almo = json.load(input_file)
 for formula in almo:
     driver.get(formula['url'])
     if not formula['descriptions']:
-        formula['descriptions'] = get_text_by_css('section.Description > div.Description__info > p')
+        formula['descriptions'] = get_text_by_css('p.Description__text')
     if not formula['key_benefits']:
-        formula['key_benefits'] = get_text_by_css('section.Description > ul > li > div.Description__consist-info > p')
+        formula['key_benefits'] = get_text_by_css('p.Description__consist-text')
     if not formula['analysis']:
-        formula['analysis'] = get_text_by_css('section.Ingredients > div > div.Ingredients__components > ul > li')
+        formula['analysis'] = get_text_by_css('li.Ingredients__el')
     if not formula['ingredients']:
-        formula['ingredients'] = get_text_by_css('section.Ingredients > div > div.Ingredients__info > div')
-    if not formula['calorie']:
-        formula['calorie'] = get_text_by_css('div.Ingredients__components > ul > li:nth-child(6) > span:nth-child(2)')
+        formula['ingredients'] = get_text_by_css('div.Ingredients__text')
     print(formula)
+input_file2 = open("./data/AlmoNature_.json", mode="w", encoding="utf-8")
 json.dump(almo, input_file2, indent=3, ensure_ascii=False)
 input_file2.close()
+input_file.close()
