44e1f1c6ccf021172c0cc011b24043f93e88642a
AndrewDongminYoo <ydm2790@gmail.com>
2021-09-03 17:07:33 +0900

    data scrapping almost done

--- utils/Fill_JSON.py
+++ utils/Fill_JSON.py
@@ -1,30 +1,63 @@
 import json
 from selenium import webdriver
+import os
 
 
 brand_list = [
-    "BOREAL",
-    "Canada Fresh",
-    "Companion Pets Classic",
-    "Dr.Link",
-    "Farmina Vet Life Feline",
-    "FirstMate",
-    "Go! Solutions",
-    "Halo pets",
-    "iti Pet Food",
-    "Little BigPaw",
-    "Mera Finest",
-    "Nulo Freestyle",
-    "Pronature Canada",
-    "Specific",
-    "Stella and Chewys",
-    "Tender and True",
-    "TIKI CATS",
+    # "A La CARTE",
+    # "Aatas Cat",
+    # "Absolute Holistic",
+    # "Addiction",
+    # "Adirondack",
+    # "Ancestry",
+    # "AnnaMaet",
+    # "Artemis",
+    # "Aujou by RAWZ",
+    # "BEST BREED",
+    # "Blue Buffalo",
+    # "Bonachibo",
+    # "Bravery Pet Food",
+    # "Canagan",
+    # "Canidae Per Foods",
+    # "Caru",
+    # "Diamond Pet Foods Premium Edge",
+    # "Dr. Clauder's Best Choice",
+    # "Earthborn",
+    # "Evanger's GrainFree",
+    # "Evolve",
+    # "Grandma mae's",
+    # "Kongo",
+    # "Lily's Kitchen",
+    # "Little BigPaw",
+    # "Marp",
+    # "Miamor (German)",
+    # "Naturliebe HappyCat",
+    # "Naturo",
+    # "North Paw",
+    # "NOW FRESH",
+    # "NutraGold",
+    # "Openfarm Korea",
+    # "Orijen Cat",
+    # "PRO PAC Ultimates",
+    # "Pro-Nutrition PureLife",
+    # "Rex Catfood",
+    "Smack Raw Hydrate Cat Food",
+    "SmartHeartGold 9 care",
+    "Steve's Real Food",
+    "Supreme Source",
+    "Taste of the Wild",
+    "TOMOJO Pet Food",
+    "TRULINE",
+    "TUSCAN NATURAL",
+    "Vet's Kitchen",
+    "Wishbone",
+    "ZEAL Canada"
 ]
 
 driver = webdriver.Chrome()
 for brand in brand_list:
-    with open(file=f"./data/0001_{brand}.json", mode="r", encoding="UTF-8", newline="") as input_file:
+    filepath = f"./data/0001_{brand}.json"
+    with open(file=filepath, mode="r", encoding="UTF-8", newline="") as input_file:
         formulas0 = json.load(input_file)
         formulas1 = sorted(formulas0, key=(lambda x: x['url']))
         for formula in formulas1:
@@ -36,5 +69,7 @@ for brand in brand_list:
                         value1 = value0[2:-2]
                         value0 = value1.split('", "')
                     formula[key] = value0
+    if os.path.exists(filepath):
+        os.remove(filepath)
     with open(file=f"./data/{brand}.json", mode="w", encoding="UTF-8", newline="") as output_file:
         json.dump(formulas1, output_file, indent=4, ensure_ascii=False, allow_nan=True)
